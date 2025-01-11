1301dc5a48bb3fbd9c3cc265c7587bee7fe457d2
ramnath98 <dongareramnath55@gmail.com>
2025-01-11 16:45:23 -0400

    add by ramnath

---
+++ zjavaprojects/src/zjavaprojects/A.java
@@ -0,0 +1,31 @@
+package zjavaprojects;
+
+public class A {
+	
+	
+	public void m1()
+	{
+		System.out.println("M1 method  A");
+	}
+	
+	public void m2()
+	{
+		System.out.println("M2 method  A");
+	}
+	
+	public void m3()
+	{
+		System.out.println("M3 method  A");
+	}
+
+	public static void main(String[] args) {
+		
+		
+		A a = new A();
+		a.m1();
+		a.m2();
+		a.m3();
+		
+	}
+
+}
